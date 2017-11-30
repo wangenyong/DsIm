0f6bbacabd8119c518cd539a824ebf7c771d84bf
wangenyong <wangenyong@gmail.com>
2017-11-30 14:14:41 +0800

    Setting theme.

--- app/src/main/java/com/dswey/dsim/ui/message/MessageFragment.java
+++ app/src/main/java/com/dswey/dsim/ui/message/MessageFragment.java
@@ -1,26 +1,36 @@
 package com.dswey.dsim.ui.message;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dswey.dsim.R;
+import com.dswey.dsim.databinding.FragmentMessageBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  * @author wangenyong
  */
 public class MessageFragment extends Fragment {
-
+    private FragmentMessageBinding mBinding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_message, container, false);
+        View view = mBinding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mBinding.toolbar.setTitle(getString(R.string.tab_message));
     }
 
     public MessageFragment() {
--- app/src/main/java/com/dswey/dsim/ui/setting/SettingFragment.java
+++ app/src/main/java/com/dswey/dsim/ui/setting/SettingFragment.java
@@ -1,13 +1,16 @@
 package com.dswey.dsim.ui.setting;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dswey.dsim.R;
+import com.dswey.dsim.databinding.FragmentSettingBinding;
 import com.dswey.dsim.ui.message.MessageFragment;
 
 /**
@@ -15,13 +18,20 @@ import com.dswey.dsim.ui.message.MessageFragment;
  * @author wangenyong
  */
 public class SettingFragment extends Fragment {
-
+    private FragmentSettingBinding mBinding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false);
+        View view = mBinding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mBinding.toolbar.setTitle(getString(R.string.tab_setting));
     }
 
     public SettingFragment() {
