359dd19a06230e870d50c13b73740820cc29a8fd
wangenyong <wangenyong@gmail.com>
2017-11-30 11:18:42 +0800

    Project init.

---
+++ app/src/main/java/com/dswey/dsim/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dswey.dsim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author wangenyong
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
